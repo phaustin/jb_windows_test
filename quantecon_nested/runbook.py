2183eea95f0c152966fe6d9a647c6db774229db2
Phil Austin <paustin@eos.ubc.ca>
2020-06-17 13:13:52 -0700

    update workspace for debug

--- quantecon_nested/runbook.py
+++ quantecon_nested/runbook.py
@@ -1,11 +1,13 @@
 from jupyter_book.commands import main
 from pathlib import Path
 import shutil
+import context
 
 if __name__ == "__main__":
-    this_dir = Path().resolve()
-    build_dir = this_dir / "mini_book/_build"
+    build_dir = context.this_dir / "mini_book/_build"
     if build_dir.is_dir():
         print(f"removing {build_dir}")
         shutil.rmtree(build_dir)
+    else:
+        print(f"couldn't fine {build_dir}")
     main()
